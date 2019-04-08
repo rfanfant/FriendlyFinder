0c26ed5bcfe0ac7fc03f83f7e70161a99439aea5
Rob Fanfant <rfanfant@hotmail.com>
2019-04-08 08:58:39 -0700

    Inititial files

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,16 +2,21 @@
 var path = require("path");
 
 // export/expose these URLS
-module.exports = function (app){
+module.exports = function (app) {
 
     // dish up our survey.html if user hits the survey URL
-    app.get("/survey", function (req,res) {
+    app.get("/survey", function (req, res) {
 
         res.sendFile(path.join(__dirname + "/../public/survey.html"));
     });
 
+    app.get("/", function (req, res) {
+        res.json(path.join(__dirname, "/../public/home.html"));
+
+    });
+
     // dish up our home.html for any other path
-    app.use(function(req,res) {
+    app.use(function (req, res) {
         res.sendFile(path.join(__dirname + "/../public/home.html"));
     });
 
